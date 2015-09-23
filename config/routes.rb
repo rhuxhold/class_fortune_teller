513353cfcabccaa7bb67dbf077313607764ff725
Russell Huxhold <rhuxhold@Russells-MacBook-Air.local>
2015-09-23 09:32:00 -0500

    class fortune teller hw answers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,31 @@ Rails.application.routes.draw do
   get("/lucky",   { :controller => "numbers", :action => "lucky_numbers" })
   get("/unlucky", { :controller => "number", :action => "unlucky" })
 
+  get("/zodiacs/aries",       { :controller =>  "zodiacs",  :action =>  "ram" })
+
+  get("/zodiacs/taurus",      { :controller =>   "zodiacs", :action =>  "bull" })
+
+  get("/zodiacs/gemini",        { :controller => "zodiacs", :action => "twins" })
+
+  get("/zodiacs/cancer",       { :controller => "zodiacs",  :action => "crab" })
+
+  get("/zodiacs/leo",        { :controller => "zodiacs",  :action => "lion" })
+
+  get("/zodiacs/virgo",      { :controller => "zodiacs", :action => "maiden" })
+
+  get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
+
+  get("/zodiacs/scorpio",      { :controller => "zodiacs", :action => "scorpion" })
+
+  get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
+
+  get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
+
+  get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
+
+  get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
+
+  get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
 
   # Uncomment each route below ONE AT A TIME and debug.
   # get("/zodiacs/aries",       { :controller =>  zodiacs,  action =>  "ram" })
